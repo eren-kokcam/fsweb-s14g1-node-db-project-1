ff305f60d34c7215b90d1d0dab64e937fecb213a
eren-kokcam <erenkokcam09@gmail.com>
2026-03-28 14:27:54 +0300

    2.2-model-middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,12 +1,29 @@
+const Account = require('./accounts-model');
+
 exports.checkAccountPayload = (req, res, next) => {
-  // KODLAR BURAYA
-  // Not: Validasyon için Yup(şu an yüklü değil!) kullanabilirsiniz veya kendiniz manuel yazabilirsiniz.
-}
+  const { name, budget } = req.body;
+  if (name === undefined || budget === undefined) {
+    res.status(400).json({ message: "name and budget are required" });
+  } else if (typeof name !== 'string' || name.trim().length < 3 || name.trim().length > 100) {
+    res.status(400).json({ message: "name of account must be between 3 and 100" });
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
+    res.status(400).json({ message: "budget of account must be a number" });
+  } else if (budget < 0 || budget > 1000000) {
+    res.status(400).json({ message: "budget of account is too large or too small" });
+  } else {
+    req.body.name = name.trim(); 
+    next();
+  }
+};
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // KODLAR BURAYA
-}
-
-exports.checkAccountId = (req, res, next) => {
-  // KODLAR BURAYA
-}
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id);
+    if (!account) {
+      res.status(404).json({ message: "account not found" });
+    } else {
+      req.account = account;
+      next();
+    }
+  } catch (err) { next(err) }
+};
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,27 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // KODLAR BURAYA
+  return db('accounts');
 }
 
 const getById = id => {
-  // KODLAR BURAYA
+  return db('accounts').where('id', id).first();
 }
 
-const create = account => {
-  // KODLAR BURAYA
+const create = async account => {
+  const [id] = await db('accounts').insert(account);
+  return getById(id);
 }
 
-const updateById = (id, account) => {
-  // KODLAR BURAYA
+const updateById = async (id, account) => {
+  await db('accounts').where('id', id).update(account);
+  return getById(id);
 }
 
-const deleteById = id => {
-  // KODLAR BURAYA
+const deleteById = async id => {
+  const deletedAccount = await getById(id);
+  await db('accounts').where('id', id).delete();
+  return deletedAccount;
 }
 
 module.exports = {
